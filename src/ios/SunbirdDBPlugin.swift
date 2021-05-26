dcdf790ed848dd5feab3329648814e217513a788
harishkumar gangula <harish@sanketika.in>
2021-05-26 15:34:33 +0530

    Add open and close methods implementation

--- src/ios/SunbirdDBPlugin.swift
+++ src/ios/SunbirdDBPlugin.swift
@@ -1,6 +1,9 @@
 import Foundation
+import SQLite3
 
 @objc(SunbirdDBPlugin) class SunbirdDBPlugin : CDVPlugin { 
+
+    var db: OpaquePointer? = nil
 
     @objc
     func init(_ command: CDVInvokedUrlCommand) {
@@ -9,12 +12,36 @@ import Foundation
 
     @objc
     func open(_ command: CDVInvokedUrlCommand) {
-        
+
+        let dbPath = command.arguments[0] as? String ?? ""
+           let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent(dbPath)
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK
+        {
+            print("error opening database")
+            pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR)
+        }
+        else
+        {
+            print("Successfully opened connection to database at \(dbPath)")
+            pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: "Successfully opened connection to database at \(dbPath)")
+        }
+        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
     }
 
     @objc
     func close(_ command: CDVInvokedUrlCommand) {
-        
+        if(sqlite3_close(db)) != SQLITE_OK
+        {
+            print("error closing the database")
+            pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR)
+        }
+        else
+        {
+            print("Successfully closed connection to database at \(dbPath)")
+            pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: "Successfully closed connection to database at \(dbPath)")
+        }
+        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
     }
 
     @objc
