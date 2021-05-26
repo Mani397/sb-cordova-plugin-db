186d14f1c3155a33d825c793c8b76ec5fcbe9571
harishkumar gangula <harish@sanketika.in>
2021-05-26 14:20:08 +0530

    Added sudo code for IOS methods

---
+++ src/ios/SunbirdDBPlugin.swift
@@ -0,0 +1,61 @@
+import Foundation
+
+@objc(SunbirdDBPlugin) class SunbirdDBPlugin : CDVPlugin { 
+
+    @objc
+    func init(_ command: CDVInvokedUrlCommand) {
+
+    }
+
+    @objc
+    func open(_ command: CDVInvokedUrlCommand) {
+        
+    }
+
+    @objc
+    func close(_ command: CDVInvokedUrlCommand) {
+        
+    }
+
+    @objc
+    func copyDatabase(_ command: CDVInvokedUrlCommand) {
+        
+    }
+
+    @objc
+    func read(_ command: CDVInvokedUrlCommand) {
+        
+    }
+     
+    @objc 
+    func insert(_ command: CDVInvokedUrlCommand) {
+        
+    }
+    
+    @objc
+    func delete(_ command: CDVInvokedUrlCommand) {
+        
+    }
+
+    @objc
+    func update(_ command: CDVInvokedUrlCommand) {
+        
+    }
+
+    @objc
+    func execute(_ command: CDVInvokedUrlCommand) {
+        
+    }
+
+    @objc
+    func beginTransaction(_ command: CDVInvokedUrlCommand) {
+        
+    }
+
+    @objc
+    func endTransaction(_ command: CDVInvokedUrlCommand) {
+        
+    }
+     
+}
+
